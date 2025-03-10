27c6ea148f889ce152360300fcc50fa00c1a5a33
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-03-10 11:55:59 -0400

    undid changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  base: '/https://github.com/ChrisNolasco135/WebProgramming2025FinalProject.git/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
